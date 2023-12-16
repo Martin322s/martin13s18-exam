47905ce775678fa4626b77718aaafa59e6f8db4b
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-12-16 14:37:56 +0200

    replace URL for UI tests

--- tests/ui/shoppingList.test.js
+++ tests/ui/shoppingList.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check shopping list page', async ({ page }) => {
-    await page.goto('http://localhost:8080/Shopping-List');
+    await page.goto('https://martin13s18-exam-8r31.onrender.com/Shopping-List');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
